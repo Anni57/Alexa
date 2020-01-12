03e4dce5b09dffc1b9c68a295e176e6a690c3ea6
Aniket Yadav <aniketyadav5757@gmail.com>
2020-01-12 21:56:52 +0530

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my Freelance Site</h1>
       <p>Know more about the developer</p>
+      <button className="btn-primary">Know more</button>
     </div>
   );
 }
